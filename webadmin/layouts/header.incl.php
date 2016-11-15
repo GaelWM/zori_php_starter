975867389ec9f2faa1bb5aec72448d684eb42e65
SILICONOVERDRIV\gael <gaelmusi0@gmail.com>
2016-11-15 16:39:20 +0200

    New Javascript classes.

--- webadmin/layouts/header.incl.php
+++ webadmin/layouts/header.incl.php
@@ -32,6 +32,15 @@
                 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
 
                 <!-- Latest compiled and minified JavaScript -->
+                <script src='js/tinyscrollbar/jquery.tinyscrollbar.js'></script>
+               <script src='js/jQuery-File-Upload-9.11.2/extras/jquery.min.js'></script>
+               <script type='text/javascript' src='js/passwordstrength.js'></script>
+               <script type='text/javascript' src='js/spin.js'></script>
+               <script type='text/javascript' src='js/jquery.inputmask.js'></script>
+               <script type='text/javascript' src='js/jquery-ui-1.8.6.custom.min.js'></script>
+               <script type='text/javascript' src='js/scripts.js'></script>
+               <!--<script type='text/javascript' src='js/maxlength.js'></script>-->
+               <script type='text/javascript' src='js/jquery.touchSwipe.min.js'></script>
                 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
                 <script src='js/default.js'></script>
 
