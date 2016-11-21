f1b99a2e74d6953821760b72c78391c57a5cc3d5
SILICONOVERDRIV\gael <gaelmusi0@gmail.com>
2016-11-21 16:12:51 +0200

    Changes on the zori.chart.cls.php

---
+++ webadmin/_framework/_zori.chart.cls.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	* By Gael Musikingala 20161121 
+	*/
+	include_once ("systems.php");
+	include_once ("_framework/_zori.control.cls.php");
+
+	class ZoriChart
+	{
+		private $HEADER;
+		public $Content;
+		private $FOOTER;
+
+		public $controls;
+   	public $db;
+		public $SystemSettings = array();
+		public $Message;
+
+		function __construct($id="myChart",$width=100,$height=100,$class="",$custom="")
+      {
+         $JS = "
+         <script>
+            <canvas id='$id' width='$width' height='$height' class='$class' $custom ></canvas>
+         ";
+		}
+
+      function setChartData(){
+         
+      }
+
+		
+
+	}
+?>
--- webadmin/project.functions.php
+++ webadmin/project.functions.php
@@ -232,6 +232,18 @@
 		gpc_extract($obj, $GLOBALS, $overrideDebug);
 	}
 
+   function get_browser_name($user_agent)
+   {
+       if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
+       elseif (strpos($user_agent, 'Edge')) return 'Edge';
+       elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
+       elseif (strpos($user_agent, 'Safari')) return 'Safari';
+       elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
+       elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
+
+       return 'Other';
+   }
+
    //UTILITY FUNCTIONS
    /*function print_rr($value, $blnString=false)
    {
