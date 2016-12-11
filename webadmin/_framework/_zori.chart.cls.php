c2547ee47ff0d68c47aafa4c7491a3ead855c763
Gael Wamba Musikingala <gaelmusi0@gmail.com>
2016-12-12 01:58:47 +0200

    20161212 - Chart updates.

--- webadmin/_framework/_zori.chart.cls.php
+++ webadmin/_framework/_zori.chart.cls.php
@@ -7,25 +7,47 @@
 
 	class ZoriChart
 	{
-		private $HEADER;
-		public $Content;
-		private $FOOTER;
+		public $width;
+		public $height;
+		public $chartType; // Line,Donut,pie.
+		public $context; // 2D or 3D
 
 		public $controls;
    		public $db;
 		public $SystemSettings = array();
 		public $Message;
 
-		function __construct($id="myChart",$width=100,$height=100,$class="",$custom="")
+		function __construct($width=0,$heigh=0,$type,$context)
       	{
-         	$JS = "
-	         	<script>
-	            	<canvas id='$id' width='$width' height='$height' class='$class' $custom ></canvas>
-	        ";
+      		$this->width = $width;
+      		$this->height = $height;
+      		$this->chartType = $type;
 		}
 
-      	function setChartData(){
-	       
+      	function renderChart($id,$class="",$custom="")
+      	{
+	       $strContent = "<canvas id='$id' width='$this->width' height='$this->height' class='$class' $custom ></canvas>";
+	       	switch ($this->chartType) {
+		       	case "Line":
+		       		$type = "Line";
+		       		break;
+		       	case "Donut":
+		       		$type = "Donut";
+		       		break;
+		       	case "Pie":
+		       		$type = "Pie";
+		       		break;
+	       	}
+
+	       $JS .="
+	       		<script>
+		       		var ctx = $('#$id').get(0).getContext('$this->context');
+					new Chart(ctx).$type(data,options);
+				</script>
+       		";
+
+       		return $strContent.$JS;
+
 	    }
 
 		
