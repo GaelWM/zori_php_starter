8cc3fc8842af762d579668c594d6f58a675bf8b4
SILICONOVERDRIV\gael <gaelmusi0@gmail.com>
2017-06-21 13:35:23 +0200

    20170621: zori chart

--- webadmin/_framework/_zori.chart.cls.php
+++ webadmin/_framework/_zori.chart.cls.php
@@ -40,36 +40,19 @@
 		       	case "Pie":
                     $this->chartType = "Pie";
 		       		break;
+                case "Bar":
+                    $this->chartType = "bar";
+                    break;
 	       	}
 
 
 	       $JS .="
                 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
-	            <script src='http://www.chartjs.org/assets/Chart.js'></script>
+	            <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.js'></script>
                 <script>
+
                     var data = 
                     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        labels : ['5','10','15','20','30','40','50','August'
-                                    ,'September','October','November','December'],
-=======
-                        labels : ['January','February','March','April','May','June','July','August','September','October','November','December'],
->>>>>>> parent of e28ce9d... 20170614: - chart class revamp - Gael
-                        
-                        datasets : 
-                            [
-                                {
-                                    fillColor : 'rgba(252,233,79,0.5)',
-                                    strokeColor : 'rgba(82,75,25,1)',
-                                    pointColor : 'rgba(166,152,51,1)',
-                                    pointStrokeColor : '#fff',
-                                    data : [65,68,75,
-                                            81,95,105,
-                                            130,120,105,
-                                            90,75,70]
-<<<<<<< HEAD
-=======
                         type: '$this->chartType',
                         data: {
                             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -103,120 +86,13 @@
                             yAxes: [{
                                 ticks: {
                                     beginAtZero:true
->>>>>>> e28ce9d26c5d8a71211143f4299f08c05e6d406c
-=======
->>>>>>> parent of e28ce9d... 20170614: - chart class revamp - Gael
                                 }
-                            ]
+                            }]
+                        }
                     }
-                
-                
-                    var options=
-                    {					
-                        //Boolean - If we show the scale above the chart data			
-                        scaleOverlay : false,
-                        
-                        //Boolean - If we want to override with a hard coded scale
-                        scaleOverride : true,
-                        
-                        //Required if scaleOverride is true 
-                        //Number - The number of steps in a hard coded scale
-                        scaleSteps : 16,
-                        //Number - The value jump in the hard coded scale
-                        scaleStepWidth : 5,
-                        //Number - The scale starting value
-                        scaleStartValue : 55,
-                        //String - Colour of the scale line	
-                        scaleLineColor : 'rgba(20,20,20,.7)',
-                        
-                        //Number - Pixel width of the scale line	
-                        scaleLineWidth : 1,
-                
-                        //Boolean - Whether to show labels on the scale	
-                        scaleShowLabels : true,
-                        
-                        //Interpolated JS string - can access value
-                        scaleLabel : '<%=value%>',
-                        
-                        //String - Scale label font declaration for the scale label
-                        scaleFontFamily : 'Arial',
-                        
-                        //Number - Scale label font size in pixels	
-                        scaleFontSize : 12,
-                        
-                        //String - Scale label font weight style	
-                        scaleFontStyle : 'normal',
-                        
-                        //String - Scale label font colour	
-                        scaleFontColor : '#666',	
-                        
-                        ///Boolean - Whether grid lines are shown across the chart
-                        scaleShowGridLines : true,
-                        
-                        //String - Colour of the grid lines
-                        scaleGridLineColor : 'rgba(0,0,0,.3)',
-                        
-                        //Number - Width of the grid lines
-                        scaleGridLineWidth : 1,	
-                        
-                        //Boolean - Whether the line is curved between points
-                        bezierCurve : true,
-                        
-                        //Boolean - Whether to show a dot for each point
-                        pointDot : true,
-                        
-                        //Number - Radius of each point dot in pixels
-                        pointDotRadius : 5,
-                        
-                        //Number - Pixel width of point dot stroke
-                        pointDotStrokeWidth : 1,
-                        
-                        //Boolean - Whether to show a stroke for datasets
-                        datasetStroke : true,
-                        
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        //Number - Pixel width of dataset stroke
-                        datasetStrokeWidth : 1,
-=======
-                    //     //Number - Pixel width of dataset stroke
-                    //     datasetStrokeWidth : 2,
->>>>>>> e28ce9d26c5d8a71211143f4299f08c05e6d406c
-=======
-                        //Number - Pixel width of dataset stroke
-                        datasetStrokeWidth : 2,
->>>>>>> parent of e28ce9d... 20170614: - chart class revamp - Gael
-                        
-                        //Boolean - Whether to fill the dataset with a colour
-                        datasetFill : true,
-                        
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        //Boolean - Whether to animate the chart
-                        animation : true,
-=======
-                    //     //Boolean - Whether to animate the chart
-                    //     animation : false,
->>>>>>> e28ce9d26c5d8a71211143f4299f08c05e6d406c
-=======
-                        //Boolean - Whether to animate the chart
-                        animation : false,
->>>>>>> parent of e28ce9d... 20170614: - chart class revamp - Gael
-                
-                        //Number - Number of animation steps
-                        animationSteps : 60,
-                        
-                        //String - Animation easing effect
-                        animationEasing : 'easeOutQuart',
-                
-                        //Function - Fires when the animation is complete
-                        onAnimationComplete : null
-                    };                
-                    
-                    
+
 		       		var ctx = $('#$this->chartID').get(0).getContext('$this->context');
-					new Chart(ctx).$this->chartType(data,options);
-					
+                    var myChart = new Chart(ctx, data,options);
 					
 				</script>
        		";
